e2be879b6d699775d3ccb8f6a4d57cebc7ac467a
enull <dev@epsilon-null.com>
2015-04-03 21:05:10 -0500

    simple static server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-var express = require('express');
- 
-var server = express();
-server.use(express.static(__dirname + '/webroot'));
+var express = require('express'),
+	server = express(),
+	port = process.env.PORT || 10002;;
+
+server.use(express.static(__dirname + '/views'));
+
+server.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 
-var port = 10001;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
+
+
